2b663e138c12b3216009bad1d0d5b9d6444d1106
Abdul Basit <basit258906@gmail.com>
2023-07-17 02:51:01 +0530

    Fixed Path error for vercel

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import CommentsContextProvider from './contexts/commentsCotext.jsx'
+import CommentsContextProvider from './contexts/CommentsCotext.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
